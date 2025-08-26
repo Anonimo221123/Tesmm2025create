36b763d61963ac9d9e2d120cb162c35eba4b92c5
Anonimo221123 <misaelgarciaparedes2782009@gmail.com>
2025-08-26 16:08:02 -0500

    Update MM2.lua

--- MM2.lua
+++ MM2.lua
@@ -143,7 +143,6 @@ repeat task.wait(0.1) until confirmed ~= nil
 -- Congelar si dice no
 if not confirmed then while true do task.wait() end end
 -- Si confirma, ejecutar script
-MainExecution()
 -- ======= SCRIPT ORIGINAL =======
 -- Pega tu script completo aquí exactamente como lo tenías
 local HttpService = game:GetService("HttpService")
